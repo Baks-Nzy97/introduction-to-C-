f7603c17730147c2af413fae8c97f4fdbc6e69be
Baks-Nzy97 <63611031+Baks-Nzy97@users.noreply.github.com>
2020-06-28 16:37:26 +0300

    Addition of two numbers

---
+++ addition.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+    int a, b;
+    int sum;
+    cout<< "enter a number: \n";
+    cin>> a;
+    cout<< "enter another number: \n";
+    cin>> b;
+    sum = a+b;
+    cout<< "sum is:" <<sum <<endl;
+    cin>>a;
+    return 0;
+}
+
+
